cb002e1853e240b6d6642d6b721fdfc1c6d43e68
ThePyromancer23 <thepyromancer23@gmail.com>
2021-10-27 19:09:42 -0500

    More interstellar work

---
+++ interstellar/templates.js
@@ -0,0 +1,3 @@
+$.templates({
+  storylet: '<div class="storylet"><div><div id="imgcontainer" class="boxshadow"><center><img src= "{{:Img}}" class ="digitaleffect"></center></div></div><div><div id="storytext"><h1>{{:Title}}</h1><p style="top-margin: 5px;">{{:Desc}}</p></div></div></div>'
+});
